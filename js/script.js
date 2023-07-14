c4c4b7972f59f6bf25b87e863cc45a44aa78804e
Di <116650495+d1-boy4h@users.noreply.github.com>
2023-07-14 14:21:28 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,135 @@
+"use strict";
+
+/* ---------------- Стрелочка наверх ---------------- */
+
+window.addEventListener('scroll', function() {
+  var headerHeight = document.querySelector('.header').offsetHeight;
+
+  if (window.scrollY > headerHeight / 2) {
+    document.querySelector('.arrow_up').classList.add('visible');
+  }
+  else {
+    document.querySelector('.arrow_up').classList.remove('visible');
+  }
+});
+
+/* ---------------- Проверка видимости (для анимаций) ---------------- */
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+     change.target.classList.add('show');
+    }
+  });
+}
+
+let options = {
+  threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.animation');
+
+for (let elm of elements) {
+  observer.observe(elm);
+}
+
+/* ---------------- Форма отправки ---------------- */
+
+$(document).ready(function() {
+
+    // E-mail Ajax Send
+    $("#order-form").submit(function() {
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "../php/mail.php",
+            data: th.serialize()
+        }).done(function() {
+            alert("Thank you!"); // Действие при успешной отправке
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
+
+});
+
+/* ---------------- Модальные окна ---------------- */
+
+let projects = 7;
+for (let k = 1; k <= projects; k++) {
+    // Открытие модального окна
+    document.getElementById("open-project-" + k).addEventListener("click", function() {
+        document.getElementById("project-" + k).classList.add("open");
+        document.getElementById("body").classList.add("locked");
+    });
+
+    // Закрытие модального окна
+    document.getElementById("close-project-" + k).addEventListener("click", function() {
+        document.getElementById("project-" + k).classList.remove("open");
+        document.getElementById("body").classList.remove("locked");
+    });
+}
+
+// Другие проекты
+document.getElementById("open-other-projects").addEventListener("click", function() {
+    document.getElementById("other-projects").classList.add("open");
+    document.getElementById("body").classList.add("locked");
+});
+
+document.getElementById("close-other-projects").addEventListener("click", function() {
+    document.getElementById("other-projects").classList.remove("open");
+    document.getElementById("body").classList.remove("locked");
+});
+
+// Заказать замер
+document.getElementById("open-order-modal").addEventListener("click", function() {
+    document.getElementById("order-modal").classList.add("open");
+    document.getElementById("body").classList.add("locked");
+});
+
+document.getElementById("close-order-modal").addEventListener("click", function() {
+    document.getElementById("order-modal").classList.remove("open");
+    document.getElementById("body").classList.remove("locked");
+});
+
+/* ---------------- Слайдер партнёров ---------------- */
+
+let offset = 0; // Смещение от левого края
+let firstClick = false; // Первое нажатие на кнопку
+
+const step = 256; // Размер шага
+const maxStep = 4352; // Максимальный шаг
+
+const sliderLine = document.querySelector('.slider_line');
+
+// Кнопка Вперёд
+document.querySelector('.slider_next').addEventListener('click', function() {
+    if (!firstClick) {
+        offset = 0;
+        firstClick = true;
+    }
+    else {
+        offset -= step;
+        if (offset < -maxStep) {
+            offset = 0;
+        }
+    }
+    sliderLine.style.left = offset + 'px';
+});
+
+// Кнопка Назад
+document.querySelector('.slider_prev').addEventListener('click', function() {
+    if (!firstClick) {
+        offset = 0;
+        firstClick = true;
+    }
+    else {
+        offset += step;
+        if (offset > 0) {
+            offset = -maxStep;
+        }
+    }
+    sliderLine.style.left = offset + 'px';
+});
